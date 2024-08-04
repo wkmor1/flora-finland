fc24bd1e9e0daf1d810753d8085374b29359a3a6
wkmor1 <willi@mmorris.email>
2024-08-04 16:25:56 +0300

    Update script

--- flora-finland.R
+++ flora-finland.R
@@ -1,19 +1,46 @@
 library(yaml)
 library(htmltools)
 library(finbif)
+library(stringr)
 
 options(finbif_cache_path = "cache", finbif_rate_limit = Inf)
 
-for (page in list.dirs("src", recursive = TRUE)[-1]) {
+checklist <- readRDS("checklist.rds")
+
+ranks <- c("class", "order", "family", "genus", "species")
+
+child_ranks <- setNames(ranks[-1], ranks[-5])
+
+parent_ranks <- setNames(ranks[-5], ranks[-1])
+
+paths <- with(
+  checklist,
+  gsub(
+    " ",
+    "_",
+    tolower(file.path("src", class, order, family, genus, scientificName))
+  )
+)
+
+for (path in paths) {
+
+  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+
+}
+
+for (page in list.dirs("src")[-1]) {
 
   content_file <- file.path(page, "content.yml")
 
   if (!file.exists(content_file)) file.create(content_file)
 
-  content <- yaml.load_file(content_file,readLines.warn = FALSE)
+  content <- yaml.load_file(content_file, readLines.warn = FALSE)
 
   content$finbifID <- basename(
-    subset(checklist, scientificName == basename(page))$taxonID
+    subset(
+      checklist,
+      gsub(" ", "_", tolower(scientificName)) == basename(page)
+    )$taxonID
   )
 
   if (!length(content$finbifID)) {
@@ -26,7 +53,67 @@ for (page in list.dirs("src", recursive = TRUE)[-1]) {
 
 }
 
+unlink("build", TRUE)
+
+dir.create("build")
+
 file.copy("src/styles.css", "build/styles.css", overwrite = TRUE)
+
+front_page <- with(
+  tags,
+  html(
+    head(
+      title("Tracheophyta"),
+      meta(
+        name = "viewport", content = "width=device-width, initial-scale=1.0"
+      ),
+      link(rel = "stylesheet", type = "text/css", href = "/styles.css")
+    ),
+    body(
+      # container
+      div(
+        class = "main",
+        # page title
+        div(
+          class = "navbar-heading",
+          span("Flora of Finland")
+        ),
+        div(
+          class = "row1",
+          div(
+            class = "col1",
+            # child taxa
+            div(
+              do.call(
+                ul,
+                lapply(
+                  mapply(
+                    a,
+                    lapply(
+                      str_to_sentence(
+                        basename(list.dirs("src", recursive = FALSE))
+                      ),
+                      span,
+                      class = "class"
+                    ),
+                    href = paste0(
+                      basename(list.dirs("src", recursive = FALSE)), "/"
+                    ),
+                    SIMPLIFY = FALSE,
+                    USE.NAMES = FALSE
+                  ),
+                  li
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+)
+
+save_html(front_page, file.path("build", "index.html"))
 
 for (page in list.files("src", recursive = TRUE, pattern = "content.yml")) {
 
@@ -34,55 +121,97 @@ for (page in list.files("src", recursive = TRUE, pattern = "content.yml")) {
 
   content <- yaml.load_file(page_file)
 
-  name <- scientific_name(content$finbifID)
+  taxon_data <- finbif_taxa(content[["finbifID"]])[["content"]][[1L]]
 
-  taxon <- dirname(page_file)
+  name <- taxon_data$scientificName
 
-  parent <- basename(dirname(taxon))
+  rank <- sub("MX.", "", taxon_data$taxonRank)
 
-  taxa <- list.dirs(taxon, recursive = FALSE, full.names = FALSE)
+  taxon <- dirname(page)
+
+  taxa <- list.dirs(dirname(page_file), recursive = FALSE, full.names = FALSE)
 
   sp_page <- with(
     tags,
     html(
       head(
         title(name),
+        meta(
+          name = "viewport", content = "width=device-width, initial-scale=1.0"
+        ),
         link(rel = "stylesheet", type = "text/css", href = "/styles.css")
       ),
       body(
+        # container
         div(
+          class = "main",
+          # page title
           div(
+            class = "navbar-heading",
+            span("Flora of Finland")
+          ),
+          div(
+            class = "row1",
+            # image
             div(
-              h1("Flora of Finland")
+              class = "col2",
+              img(src = content$images[[1L]][["file"]])
             ),
-            if (length(taxa)) div(
-              do.call(
-                ul,
-                lapply(
-                  mapply(
-                    a,
-                    taxa,
-                    href = paste0(taxa, "/"),
-                    SIMPLIFY = FALSE,
-                    USE.NAMES = FALSE
-                  ),
-                  li
+            div(
+              class = "col1",
+              # parent taxon
+              if (basename(dirname(taxon)) != ".") {
+                div(
+                  a(
+                    "\u2190",
+                    span(
+                      str_to_sentence(basename(dirname(taxon))),
+                      class = parent_ranks[[rank]]
+                    ),
+                    href = file.path("", dirname(taxon))
+                  )
+                )
+              } else {
+                div(
+                  a("\u2190", span("Tracheophyta", class ="phylum"), href = "/")
+                )
+              },
+              # taxon name
+              div(
+                h1(
+                  class = "page-title",
+                  span(name, class = rank)
+                ),
+                if (!is.null(content$vernacularName)) {
+                  h2(content$vernacularName, class = "subtitle")
+                }
+              ),
+              # child taxa
+              if (length(taxa)) div(
+                do.call(
+                  ul,
+                  lapply(
+                    mapply(
+                      a,
+                      lapply(
+                        gsub("_", " ", str_to_sentence(taxa)),
+                        span,
+                        class = child_ranks[[rank]]
+                      ),
+                      href = paste0(taxa, "/"),
+                      SIMPLIFY = FALSE,
+                      USE.NAMES = FALSE
+                    ),
+                    li
+                  )
                 )
               )
-            ),
-            div(
-              img(src = content$images[[1L]][["file"]], width = 500)
             )
           ),
-          if (parent != "build") div(
-            h3(parent)
-          ),
-          div(
-            h2(name)
-          ),
-          div(
-            h2("Description"),
-            p(content$description)
+          # description
+          if (!is.null(content$description)) div(
+            class = "row2",
+            p(class = "description", content$description)
           )
         )
       )
